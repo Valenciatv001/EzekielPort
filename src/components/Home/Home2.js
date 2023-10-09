509f5b2c064d651d7c9e1695dec0d091fc7a67d7
Valenciatv001 <67506940+Valenciatv001@users.noreply.github.com>
2023-10-09 14:15:55 +0100

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import myImg from "../../Assets/avatar.svg";
-import myImg from "../../Assets/ezekkk.jpg"
+import myImg from "../../Assets/avatar.svg";
+// import myImg from "../../Assets/ezekkk.jpg"
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
