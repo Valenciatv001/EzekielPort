4330972c2840eaa6477ebf776793e727f1426999
ezekiel <irechukwuezekiel@gmail.com>
2024-06-11 00:20:52 +0100

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/AgwuEzekielResume.pdf.pdf"
+import pdf from "../../Assets/AgwuEzekiel.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -50,7 +50,7 @@ function ResumeNew() {
         </Row>
       </Container>
     </div>
-    );
+  );
 }
 
 export default ResumeNew;
